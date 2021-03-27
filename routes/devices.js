22bc8fbacd4bf16bfd6ae703192b035b4148ab53
Divyansh Gupta <divyanshg809@gmail.com>
2021-03-28 00:43:58 +0530

    Update devices.js

--- routes/devices.js
+++ routes/devices.js
@@ -1,24 +1,20 @@
 const express = require('express')
 const router = express.Router()
-
 const {
     checkAuthenticated,
     checkNotAuthenticated
 } = require('../authorizers/users')
-
 var {
     getDevices,
     getDevice,
     updateDashboardId
 } = require('../models/dashboard-render-helper')
-
 router.get('/:projectID', checkAuthenticated, async (req, res) => {
     res.render('devices/index.ejs', {
         devices: await getDevices(req.params.projectID, req.user.id),
         projectID:req.params.projectID
     })
 })
-
 router.get('/:device/:tag/:projectID', checkAuthenticated, async (req, res) => {
     res.render('devices/device.ejs', {
         owner: req.user.id,
@@ -26,5 +22,4 @@ router.get('/:device/:tag/:projectID', checkAuthenticated, async (req, res) => {
         projectID:req.params.projectID
     })
 })
-
 module.exports = router
